e97a714197196e11a384b49ea0e5131bd4b7a24d
acid7937 <hsgs5891@gmail.com>
2023-04-22 01:09:50 +0900

    태그 색상 설정

--- pages/components/projects/project-item.js
+++ pages/components/projects/project-item.js
@@ -6,6 +6,7 @@ export default function ProjectItem({ data }) {
   }
 
   const title = data.properties.Projects?.title?.[0]?.plain_text || "";
+  const member = data.properties.Participants?.rich_text?.[0]?.plain_text || "";
   const description = data.properties.Description?.rich_text?.[0]?.plain_text || "";
   const github = data.properties.Github?.url || "";
   const blog = data.properties.Blog?.url || "";
@@ -52,19 +53,56 @@ export default function ProjectItem({ data }) {
             
             <h1 className="text-2xl font-bold">{title}</h1>
             <h3 className="mt-4 text-xl">{description}</h3>
-            <a href={github}>깃허브 바로가기</a>
-            <a href={blog}>관련 게시글 바로가기</a>
+            <h5 className="mt-4 text-xl">{member}</h5>
+
+            <a href={github} className="mt-1">깃허브 바로가기</a>
+            <a href={blog} className="mt-1">관련 게시글 바로가기</a>
 
             <p className="my-1 ">
                 작업기간 : {start} ~ {end} ({calculatedPeriod(start, end)}일)
                 </p>
 
             <div className="flex items-start mt-2">
-            {tags.map((aTag)=>(
+ {/*            {tags.map((aTag)=>(
                 <h1 className="px-2 py-1 mr-2 rounded-md bg-sky-200 dark:bg-sky-700 w-30" 
                 key={aTag.id}> {aTag.name} </h1>
                 
-            ))}
+            ))} */}
+
+{tags.map((aTag) => (
+  <h1
+    className={`px-2 py-1 mr-2 rounded-md w-30 ${
+      aTag.color === "pink"
+        ? "bg-pink-200 dark:bg-pink-700"
+
+        :aTag.color === "blue"
+        ? "bg-blue-200 dark:bg-blue-700"
+
+        :aTag.color === "red"
+        ? "bg-red-200 dark:bg-red-700"
+        
+        :aTag.color === "orange"
+        ? "bg-orange-200 dark:bg-orange-700"
+
+        : aTag.color === "gray"
+        ? "bg-gray-200 dark:bg-gray-700"
+
+        : aTag.color === "green"
+        ? "bg-green-200 dark:bg-green-700"
+
+        : aTag.color === "purple"
+        ? "bg-purple-200 dark:bg-purple-700"
+
+        : aTag.color === "yellow"
+        ? "bg-yellow-200 dark:bg-yellow-700"
+
+        : "bg-sky-200 dark:bg-sky-700"
+    }`}
+    key={aTag.id}
+  >
+    {aTag.name}
+  </h1>
+))}
 
 
             </div>
