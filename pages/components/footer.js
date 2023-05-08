e7b14e649fab7c7eada3b0993dd53bf49cf1252f
acid7937 <hsgs5891@gmail.com>
2023-05-09 07:06:45 +0900

    페북인스타제거

--- pages/components/footer.js
+++ pages/components/footer.js
@@ -69,7 +69,7 @@ export default function Footer(){
   </svg>
 </a>
 
-<a href="https://www.instagram.com/stinson5891" className="ml-3 text-gray-500">
+{/* <a href="https://www.instagram.com/stinson5891" className="ml-3 text-gray-500">
                         <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
                             <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
                             <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
@@ -80,7 +80,7 @@ export default function Footer(){
       <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
          <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
        </svg>
-        </a>
+        </a> */}
 
 
 
